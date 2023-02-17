95723a866fda16a94f845ef3a3379dabd17a2de3
BrunoUzi <hirwabezabruno@gmail.com>
2023-02-17 10:41:49 -0800

    c file

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,10 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-	puts"\("Programming is like building a multilingual");
 	return (0);
 }
