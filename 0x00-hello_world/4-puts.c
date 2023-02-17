ba026e6a0e91c4cb74b0a952dbbe3ecd6c014532
BrunoUzi <hirwabezabruno@gmail.com>
2023-02-17 08:09:58 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include<studio.h>
-
-  int main(void)
-      
-           {
-	   puts("\" programming is like building a multilingual puzzle");              
-	   return (0);
-	   }
-
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}		
