17fa2c4ba9cda49b163a35545381b650d4db034e
BrunoUzi <hirwabezabruno@gmail.com>
2023-02-17 09:32:25 -0800

    c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include<studio>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+	int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+
+	puts ("\"Programming is like building a multilingual");
 	return (0);
 }		
