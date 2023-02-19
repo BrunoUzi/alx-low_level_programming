90943b0ffefda7b9050385e5daa90561c55c9cd9
BrunoUzi <hirwabezabruno@gmail.com>
2023-02-19 06:01:17 -0800

    c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
-/**i
+/**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" -DoraKorpar, 2015-10-19\n", stdout);
 	return (1);
 }
