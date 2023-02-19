b36af4b6e239d19683e4557513998558f8e288fe
BrunoUzi <hirwabezabruno@gmail.com>
2023-02-19 04:52:22 -0800

    c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - Entry point
+ * main - A c program that prints a line to the standard error
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
